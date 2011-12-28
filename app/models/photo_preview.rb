020fa9d8125f40028f7ac553c0ae3583ebba2b13
dongbin.li <mike.d.1984@gmail.com>
2011-12-28 12:46:07 +0800

    fix image download

--- app/models/photo_preview.rb
+++ app/models/photo_preview.rb
@@ -3,8 +3,8 @@ class PhotoPreview < ActiveRecord::Base
   mount_uploader :photo, PhotoPreviewUploader
 
   def process_image(url)
-    result = (self.remote_photo_url = url)
-    self.update_attribute :photo,result
+    self.remote_photo_url = url
+    self.update_attribute :photo,self.photo
   end
 
   handle_asynchronously :process_image
